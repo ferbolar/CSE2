0238127312d2fd9bd47bfbe3c36e8bd9c0787130
ferbolar <feb217@lehigh.edu>
2014-08-27 20:41:55 +0000

    updated comment

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////
+//Fernando Bolanos
+//Demo 0
+//Hello World Java Program
+//
+//  compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld.java
+//
+//   Define class
+public class HelloWorld{
+    
+// add main method
+    public static void main(String[ ] args) {
+    
+// print statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("Testing");
+        
+        //test
+    }
+}

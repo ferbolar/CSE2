dd5d0b319b66ed5c96278f66acb484e0c7b88888
ferbolar <feb217@lehigh.edu>
2014-08-28 20:47:27 +0000

    updated comment

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,33 @@
+/////////////////////////////////////////////////
+//Fernando Bolanos
+//HW01
+//Welcome Class HW Program
+//
+//  Compile the program
+//      javac WelcomeClass.java
+//
+//  Run the program
+//      java WelcomeClass.java
+//
+// Define Class
+public class WelcomeClass{
+    
+//  Add Main Method
+        public static void main(String[ ] args) {
+            
+// Print Statement
+            System.out.println("   ----------------   ");
+            System.out.println("   |    WELCOME   |   ");
+            System.out.println("   ----------------   ");
+            System.out.println("   ^  ^  ^  ^  ^  ^   ");
+            System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+            System.out.println(" |-F--E--B--2--1--7-| ");
+            System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ / ");
+            System.out.println("   v  v  v  v  v  v   ");
+            System.out.println("This summer I had the opportunity to inter in a private");
+            System.out.println("equity firm in Costa Rica, this was an amazing experience.");
+            
+            
+        }
+        
+}

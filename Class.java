225b4e7e419bf830aaa488ea09d43c9e89cefb11
ferbolar <feb217@lehigh.edu>
2014-10-15 20:44:01 +0000

    updated comment

---
+++ Class.java
@@ -0,0 +1,12 @@
+public class nClass{
+    public static void main(String[] args){
+        int i = 0, j = 15, k = 7;
+        double x = -40.0, y = 0.0, z= 3.14;
+        String s = "fall", t = "break";
+        boolean p = false, q = true;
+        char c = "D";
+        
+        j/k > z-1;
+    
+    }
+}

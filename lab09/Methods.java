9925e6fbeac9bfb88e0e327f3234b78afd7b9c09
ferbolar <feb217@lehigh.edu>
2014-11-17 22:31:32 +0000

    updated comment

--- lab09/Methods.java
+++ lab09/Methods.java
@@ -19,34 +19,34 @@ public class Methods{
                        	", and "+c+" are in ascending order");
     }
   
-    public static int getInt(Scanner scan){
+    public static int getInt(Scanner scan){//declare method
         System.out.print("Enter an int: ");
-        while(!scan.hasNextInt()){
+        while(!scan.hasNextInt()){//check if it is an integer and repeat if they dont
             scan.next();
             System.out.print("You did not enter an int, try again. ");
         }
-        int a = scan.nextInt();
-        return a;
+        int a = scan.nextInt();//store the int
+        return a;//return inputs
     }
-    public static int larger(int a, int b){
-        if(a > b){
+    public static int larger(int a, int b){//declare method
+        if(a > b){//check if the first int is larger
             return a;
         }
-        else{
+        else{//if it is not larger
             return b;
         }
     }
-    public static String ascending(int a, int b, int c){
-        if (a < b){
-            if (b < c){
-                return "True";
+    public static String ascending(int a, int b, int c){//declare method to see if it is ascending
+        if (a < b){//check if the first one is smaller than the second one
+            if (b < c){//check if the second one is smaller than the third one
+                return "True";//return it is true it is ascending
             }
             else{
-                return "False";
+                return "False";//return it is not ascending
             }
         }
         else{
-            return "False";
+            return "False";//return it is not ascending
         }
     }
 }
